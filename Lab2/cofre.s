615225a695ee8e11608b5f36a5b2eabdff74f164
vayakim <81124746+vayakim@users.noreply.github.com>
2023-10-22 19:41:32 -0300

    estados do lcd e cofre

--- Lab2/cofre.s
+++ Lab2/cofre.s
@@ -7,7 +7,7 @@
         THUMB                        ; Instruções do tipo Thumb-2
 ; -------------------------------------------------------------------------------
 ; Declarações EQU - Defines
-ENDERECO_BASE EQU 0x20000400
+ENDERECO_BASE_SENHA EQU 0x20000400
 ;<NOME>         EQU <VALOR>
 ; -------------------------------------------------------------------------------
 ; Área de Dados - Declarações de variáveis
@@ -39,14 +39,24 @@ ENDERECO_BASE EQU 0x20000400
 		IMPORT PortL_Input          ; Permite chamar PortL_Input de outro arquivo
 		
 ;“Cofre aberto, digite nova senha para fechar o cofre”.
-;cofre aberto = cfr abt
-COFRE_ABERTO DCB 0x43, 0x66, 0x72, 0x20 ,0x61,0x62,0x74,0x2C, 0x00
+cofre_aberto = 0x43, 0x6F, 0x66, 0x72, 0x65, 0x20, 0x61, 0x62, 0x65, 0x72, 0x74, 0x6F, 0x2C, 0x20, 0x64, 0x69, 0x67, 0x69, 0x74, 0x65, 0x20, 0x6E, 0x6F, 0x76, 0x61, 0x20, 0x73, 0x65, 0x6E, 0x68, 0x61, 0x20, 0x00
 
-;digite nova senha = dgt nv snh
-DIGITE_NOVA_SENHA DCB 0x64, 0x67, 0x74, 0x20, 0x6E, 0x76, 0x20, 0x73, 0x6E, 0x68, 0x00
-;
+;"Cofre fechando..."
+cofre_fechando = 0x43, 0x6F, 0x66, 0x72, 0x65, 0x20, 0x66, 0x65, 0x63, 0x68, 0x61, 0x6E, 0x64, 0x6F, 0x00
 
-init_string = 0x43, 0x6F, 0x66, 0x72, 0x65, 0x20, 0x61, 0x62, 0x65, 0x72, 0x74, 0x6F, 0x2C, 0x20, 0x64, 0x69, 0x67, 0x69, 0x74, 0x65, 0x20, 0x6E, 0x6F, 0x76, 0x61, 0x20, 0x73, 0x65, 0x6E, 0x68, 0x61, 0x20, 0x00
+;"Cofre fechado!"
+cofre_fechado = 0x43, 0x6F, 0x66, 0x72, 0x65, 0x20, 0x66, 0x65, 0x63, 0x68, 0x61, 0x64, 0x6F, 0x21, 0x00
+
+;estados do cofre -> R7
+;0x0 = aberto
+;0x1 = fechando
+;0x2 = fechado
+
+;estados do LCD -> R8
+;0x0 = parado
+;0x1 = correndo
+
+
 ; -------------------------------------------------------------------------------
 ; Função main()
 Start  		
@@ -54,6 +64,8 @@ Start
 	BL SysTick_Init
 	BL GPIO_Init                ;Chama a subrotina que inicializa os GPIO
 	
+	mov r7, #0x1
+	mov r8, #0x0
 	mov r10, #1    ;equivale à linha atual
 	mov r11, #0 ;equivale à coluna atual da linha
 	
@@ -78,20 +90,32 @@ LCD_Init
     bl  LCD_SendCommand
 	bl Delay_40us
 	
-    BL Cofre_Aberto
-
-
-
-MainLoop
-	mov r10, #1
+	cmp r7, #0x0
+	beq Cofre_Aberto
 	
+	cmp r7, #0x1
+	beq Cofre_Fechando
+	
+	cmp r7, #0x2
+	beq Cofre_Fechado
+
+MainLoop	
 	BL LCD_Update
 	B MainLoop
 	
 LCD_Update
 	push{lr}
+	; ve se o letreiro esta em modo parado
+	cmp r8, #0x0
+	beq mantem_parado
 	
-    bl Cofre_Aberto
+	movs r0, #0x18
+	bl LCD_SendCommand
+	bl Delay_40us
+	movs r0, #200
+	bl SysTick_Wait1ms
+    
+mantem_parado
 	pop{lr}
 	BX LR
 
@@ -134,8 +158,8 @@ LCD_SendData
 	
     ;Colocar o comando nos pinos de dados do LCD
 	BL PortK_Output
-	mov r0, #200
-	BL SysTick_Wait1ms
+	;mov r0, #200
+	;BL SysTick_Wait1ms
 	; Configurar RS (Register Select) para 1 (Dado) e RW (Read/Write) para 0 (escrita)
     MOV R1, #2_00000001
 	
@@ -156,20 +180,38 @@ LCD_SendData
     BX LR
 
 ; Função Cofre_Aberto
-; Rotina que envia a mensagem "COFRE ABERTO" para o LCD se o cofre estiver aberto
+; Rotina que envia a mensagem inicial de cofre aberto para o LCD 
 ; Parâmetro de entrada: Não tem
 ; Parâmetro de saída: Não tem
 Cofre_Aberto
-	PUSH{LR}
-	
-	LDR R5, =init_string
+	LDR R5, =cofre_aberto
 	BL STRING_TO_LCD
+	B MainLoop
 	
-	POP{LR}
+; Função Cofre_Fechando
+; Rotina que envia a mensagem de cofre fechando para o LCD quando o sistema recebe uma senha
+; Parâmetro de entrada: Não tem
+; Parâmetro de saída: Não tem
+Cofre_Fechando
+	LDR R5, =cofre_fechando
+	BL STRING_TO_LCD
+	mov r0, #5000
+	bl SysTick_Wait1ms
+	movs r0, #0x01      ; Clear Display
+    bl  LCD_SendCommand
+	bl Delay_1640us
+	mov r11, #0
+	b Cofre_Fechado
 	
-	BX LR
+; Função Cofre_Fechado
+; Rotina que envia a mensagem de cofre fechado para o LCD
+; Parâmetro de entrada: Não tem
+; Parâmetro de saída: Não tem
+Cofre_Fechado
+	LDR R5, =cofre_fechado
+	BL STRING_TO_LCD
+	B MainLoop
 	
-
 ; Função STRING_TO_LCD
 ; Rotina para enviar uma string para o LCD
 ; Parâmetro de entrada: R5 - o rótulo contendo a mensagem a ser enviada
@@ -180,6 +222,7 @@ STRING_TO_LCD
 iterateLoop
 	b test_end_line
 
+;envia normalmente a informacao contida na string
 continue_sendData
 	ldrb r0, [r5, r6]  
 	cmp r0, #0         
@@ -189,26 +232,25 @@ continue_sendData
 	add r6, #1
 	b iterateLoop
 
+;verifica se a linha terminou utilizando um contador auxiliar em r11
 test_end_line
 	cmp r11, #0x10
 	beq atualiza_modo
 	b continue_sendData
 
+;quando chega no final da linha, faz com que a mensagem se desloque para a esquerda quando um novo caractere eh inserido
 atualiza_modo
 	movs r0, #0x07
+	movs r8, #0x1
 	bl LCD_SendCommand
 	bl Delay_40us
-	
+
+;atualiza a flag de linha, para fazer o letreiro correr
 	mov r10, #2
 	b continue_sendData
-	
+
+;quando a palavra acaba, shifta o display se o LCD esta em modo letreiro, ou mantem ele parado
 end_word
-	movs r0, #0x18
-	bl LCD_SendCommand
-	bl Delay_40us
-	movs r0, #200
-	bl SysTick_Wait1ms
-	b end_word
 	POP{LR}
 	BX LR
 
